9908ed37591ed67f1bc2b2f976b54612e6e9453f
alex <alex@k.com>
2026-07-23 15:04:07 +0200

    add config for sddm (not in .config, but /etc and /usr)

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -267,26 +267,25 @@ hl.device({
 local mainMod = "SUPER" -- Sets "Windows" key as main modifier
 
 -- Example binds, see https://wiki.hypr.land/Configuring/Basics/Binds/ for more
-hl.bind(mainMod .. " + T", hl.dsp.exec_cmd(terminal))
-local closeWindowBind = hl.bind(mainMod .. " + W", hl.dsp.window.close())
--- closeWindowBind:set_enabled(false)
-hl.bind(mainMod .. " + SHIFT + W", hl.dsp.window.kill())
-hl.bind(mainMod .. " + M", hl.dsp.exec_cmd("hyprshutdown -t 'Logging out...'"))
-hl.bind(mainMod .. " + SHIFT + M", hl.dsp.exec_cmd("hyprshutdown -t 'Shutting down...' --post-cmd 'shutdown -P 0'"))
+hl.bind(mainMod .. " + T",                hl.dsp.exec_cmd(terminal))
+hl.bind(mainMod .. " + W",                hl.dsp.window.close())
+hl.bind(mainMod .. " + SHIFT + W",        hl.dsp.window.kill())
+hl.bind(mainMod .. " + M",                hl.dsp.exec_cmd("hyprshutdown -t 'Logging out...'"))
+hl.bind(mainMod .. " + SHIFT + M",        hl.dsp.exec_cmd("hyprshutdown -t 'Shutting down...' --post-cmd 'shutdown -P 0'"))
 hl.bind(mainMod .. " + CTRL + SHIFT + M", hl.dsp.exec_cmd("hyprshutdown -t 'Restarting...' --post-cmd 'reboot'"))
-hl.bind(mainMod .. " + L", hl.dsp.exec_cmd("hyprlock"))
--- hl.bind(mainMod .. " + F", hl.dsp.exec_cmd(fileManager))
-hl.bind(mainMod .. " + B", hl.dsp.exec_cmd(browser))
-hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(email))
-hl.bind(mainMod .. " + V", hl.dsp.window.float({ action = "toggle" }))
-hl.bind(mainMod .. " + R", hl.dsp.exec_cmd(menu))
-hl.bind(mainMod .. " + P", hl.dsp.window.pseudo())
-hl.bind(mainMod .. " + J", hl.dsp.layout("togglesplit"))    -- dwindle only
-hl.bind(mainMod .. " + K", hl.dsp.layout("swapsplit"))    -- dwindle only
-hl.bind(mainMod .. " + F", hl.dsp.window.fullscreen({mode = "fullscreen", action = "toggle"}))
-hl.bind(mainMod .. " + SHIFT + F", hl.dsp.window.fullscreen({mode = "maximized", action = "toggle"}))
-hl.bind(mainMod .. " + Y", hl.dsp.exec_cmd(freetube))
-hl.bind(mainMod .. " + C", hl.dsp.exec_cmd(freecad))
+hl.bind(mainMod .. " + L",                hl.dsp.exec_cmd("hyprlock"))
+-- hl.bind(mainMod .. " + F",                hl.dsp.exec_cmd(fileManager))
+hl.bind(mainMod .. " + B",                hl.dsp.exec_cmd(browser))
+hl.bind(mainMod .. " + E",                hl.dsp.exec_cmd(email))
+hl.bind(mainMod .. " + V",                hl.dsp.window.float({ action = "toggle" }))
+hl.bind(mainMod .. " + R",                hl.dsp.exec_cmd(menu))
+hl.bind(mainMod .. " + P",                hl.dsp.window.pseudo())
+hl.bind(mainMod .. " + J",                hl.dsp.layout("togglesplit"))    -- dwindle only
+hl.bind(mainMod .. " + K",                hl.dsp.layout("swapsplit"))    -- dwindle only
+hl.bind(mainMod .. " + F",                hl.dsp.window.fullscreen({mode = "fullscreen", action = "toggle"}))
+hl.bind(mainMod .. " + SHIFT + F",        hl.dsp.window.fullscreen({mode = "maximized", action = "toggle"}))
+hl.bind(mainMod .. " + Y",                hl.dsp.exec_cmd(freetube))
+hl.bind(mainMod .. " + C",                hl.dsp.exec_cmd(freecad))
 
 -- Move focus with mainMod + arrow keys
 hl.bind("CTRL + SHIFT + H", hl.dsp.focus({ direction = "left" }))
@@ -298,8 +297,8 @@ hl.bind("CTRL + SHIFT + J", hl.dsp.focus({ direction = "down" }))
 -- Move active window to a workspace with mainMod + SHIFT + [0-9]
 for i = 1, 10 do
     local key = i % 10 -- 10 maps to key 0
-    hl.bind(mainMod .. " + " .. key,             hl.dsp.focus({ workspace = i}))
-    hl.bind(mainMod .. " + SHIFT + " .. key,     hl.dsp.window.move({ workspace = i }))
+    hl.bind(mainMod .. " + " .. key,         hl.dsp.focus({ workspace = i}))
+    hl.bind(mainMod .. " + SHIFT + " .. key, hl.dsp.window.move({ workspace = i }))
 end
 
 -- Example special workspace (scratchpad)
@@ -309,20 +308,20 @@ hl.bind(mainMod .. " + SHIFT + S", hl.dsp.window.move({ workspace = "special:mag
 -- Scroll through existing workspaces with mainMod + scroll
 hl.bind(mainMod .. " + mouse_down", hl.dsp.focus({ workspace = "e+1" }))
 hl.bind(mainMod .. " + mouse_up",   hl.dsp.focus({ workspace = "e-1" }))
-hl.bind(mainMod .. " + left", hl.dsp.focus({ workspace = "e+1" }))
-hl.bind(mainMod .. " + right",   hl.dsp.focus({ workspace = "e-1" }))
+hl.bind(mainMod .. " + left",       hl.dsp.focus({ workspace = "e+1" }))
+hl.bind(mainMod .. " + right",      hl.dsp.focus({ workspace = "e-1" }))
 
 -- Move/resize windows with mainMod + LMB/RMB and dragging
 hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(),   { mouse = true })
 hl.bind(mainMod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true })
 
 -- Laptop multimedia keys for volume and LCD brightness
-hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"), { locked = true, repeating = true })
-hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),      { locked = true, repeating = true })
-hl.bind("XF86AudioMute",        hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),     { locked = true, repeating = true })
-hl.bind("XF86AudioMicMute",     hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),   { locked = true, repeating = true })
-hl.bind("XF86MonBrightnessUp",  hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"),                  { locked = true, repeating = true })
-hl.bind("XF86MonBrightnessDown",hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"),                  { locked = true, repeating = true })
+hl.bind("XF86AudioRaiseVolume",  hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"), { locked = true, repeating = true })
+hl.bind("XF86AudioLowerVolume",  hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),      { locked = true, repeating = true })
+hl.bind("XF86AudioMute",         hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),     { locked = true, repeating = true })
+hl.bind("XF86AudioMicMute",      hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),   { locked = true, repeating = true })
+hl.bind("XF86MonBrightnessUp",   hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"),                  { locked = true, repeating = true })
+hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"),                  { locked = true, repeating = true })
 
 -- Requires playerctl
 hl.bind("XF86AudioNext",  hl.dsp.exec_cmd("playerctl next"),       { locked = true })
@@ -331,8 +330,8 @@ hl.bind("XF86AudioPlay",  hl.dsp.exec_cmd("playerctl play-pause"), { locked = tr
 hl.bind("XF86AudioPrev",  hl.dsp.exec_cmd("playerctl previous"),   { locked = true })
 
 -- Screenshots
-hl.bind("PRINT",  hl.dsp.exec_cmd("hyprshot -m window"))
-hl.bind("SHIFT + PRINT",  hl.dsp.exec_cmd("hyprshot -m output -m DP-1"))
+hl.bind("PRINT",         hl.dsp.exec_cmd("hyprshot -m window"))
+hl.bind("SHIFT + PRINT", hl.dsp.exec_cmd("hyprshot -m output -m DP-1"))
 
 --------------------------------
 ---- WINDOWS AND WORKSPACES ----
@@ -379,7 +378,6 @@ hl.window_rule({
 hl.window_rule({
     name  = "move-hyprland-run",
     match = { class = "hyprland-run" },
-
     move  = "20 monitor_h-120",
     float = true,
 })
