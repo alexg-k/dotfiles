d0a6d097a2d3664a802d0a5bd18eb057f1690ecf
alex <alex@k.com>
2026-08-30 08:10:14 +0200

    add fish shell and make it default in kitty

---
+++ .config/fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    fish_config theme choose catppuccin-mocha --color-theme=dark
+
+    # Show every directory name in full while retaining ~ for the home directory.
+    set -g fish_prompt_pwd_dir_length 0
+
+    fish_vi_key_bindings
+    set -g fish_cursor_default block
+    set -g fish_cursor_insert line
+    set -g fish_cursor_replace_one underscore
+    set -g fish_cursor_visual block
+    set -g fish_greeting
+end
